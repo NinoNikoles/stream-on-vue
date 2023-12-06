e5d6c8a6da74fc5db144ad78a6f95822e9a031c1
Nino Nikoles <nnikoles@gmail.com>
2023-12-06 11:35:33 +0100

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production' ? '/stream-on-vue/' : '/',
     transpileDependencies: true,
 };
 
