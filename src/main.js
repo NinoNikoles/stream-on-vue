214115bace21cb497c2ee972b5125f6211e0a7a6
Nino Nikoles <nnikoles@gmail.com>
2024-03-26 17:34:23 +0100

    Update main.js
    Seiteneinstellungen und User infos (nur von sich selbst) sind jetzt global erreichbar

--- src/main.js
+++ src/main.js
@@ -1,17 +1,18 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import axios from 'axios';
 
 const app = createApp(App);
 
 // Setze globale Konfigurationsoptionen (wie productionTip)
 app.config.globalProperties.productionTip = false; // Setze den Wert auf true oder false, wie benötigt
+const mainURL = window.location.protocol + '//' + window.location.hostname;
 
 function loadImg(img, size = 'original') {
-    if ( !img || !size) return '/build/css/images/img_preview.webp';
-    // return '';
-    // return '/build/css/images/img_preview.webp';
-    return `http://image.tmdb.org/t/p/${size}${img}`;
+    if ( !img || !size) return '/build/css/images/placeholder.webp';
+    return '/build/css/images/placeholder.webp';
+    // return `http://image.tmdb.org/t/p/${size}${img}`;
 }
 
 function truncate(string, length = 100, append = "...") {
@@ -21,20 +22,100 @@ function truncate(string, length = 100, append = "...") {
     return string;
 }
 
-const mainURL = window.location.protocol + '//' + window.location.hostname;
+
+async function fetchSessionStatus(mainURL) {
+    async function getSession(mainURL) {
+        return new Promise((resolve, reject) => {
+            try {
+                const response = axios.get(`${mainURL}:3000/api/db/session`, { withCredentials: true });
+                resolve(response);
+            } catch(err) {
+                reject(err);
+            }            
+        });
+    }
+
+    async function getCurrentUserInfos(mainURL, user) {
+        try {
+            var response = await axios.get(`${mainURL}:3000/api/db/getUser?userID=${user.id}`);
+            var userData = response.data[0];
+            var userInfos = {
+                activeImg: '',
+                uploads: JSON.parse(userData.uploads),
+                volume: userData.media_volume
+            };
+
+            userInfos.activeImg = `/media/avatar.webp`;
+            if ( userData.img !== '-1' ) userInfos.activeImg = `/media/user_uploads/${user.id}/${userData.img}`;
+
+            return userInfos;
+        } catch (error) {
+            return error;
+        }
+    }
+
+    try {
+        const response = await getSession(mainURL);
+        if ( response.data.user ) {
+            var userData = {
+                isLoggedIn: response.data.user.isLoggedIn,
+                id: response.data.user.id,
+                username: response.data.user.name,
+                role: response.data.user.role,
+                activeImg: '',
+                uploads: [],
+                volume: null
+            }
+
+            try {
+                var userDbData = await getCurrentUserInfos(mainURL, userData);
+                userData.activeImg = userDbData.activeImg;
+                userData.uploads = userDbData.uploads;
+                userData.volume = userDbData.volume;
+
+                return new Promise((resolve) => {
+                    resolve(userData);
+                });
+            } catch(err) {
+                return err;
+            }
+        }               
+    } catch (error) {
+        return error;
+    }         
+}
+
+async function fetchPageSettings(mainURL) {
+    try {
+        var response = await axios.get(`${mainURL}:3000/api/db/getSettings`);
+        return new Promise((resolve) => {
+            resolve(response.data);
+        });
+    } catch (error) {
+        return error;
+    }   
+}
+
+
+var user = await fetchSessionStatus(mainURL);
+var pageSettings = await fetchPageSettings(mainURL);
+
 app.config.globalProperties.$mainURL = mainURL;
 app.config.globalProperties.$loadImg = loadImg;
 app.config.globalProperties.$truncate = truncate;
+app.config.globalProperties.$user = user;
+app.config.globalProperties.$pageSettings = pageSettings;
 
 app.mixin({
-    updated() {
-        // document.querySelectorAll('img[data-img]').forEach(function(el) {
-        //     el.setAttribute('src', el.getAttribute('data-img'));
-        //     el.removeAttribute('data-img');
-        // });
-        
-        this.$nextTick(() => {
+    updated() {        
+        this.$nextTick(async () => {
             window.initCustomJS();
+            
+            var user = await fetchSessionStatus(mainURL);
+            var pageSettings = await fetchPageSettings(mainURL);
+            
+            app.config.globalProperties.$user = user;
+            app.config.globalProperties.$pageSettings = pageSettings;
         });
     }
 });
