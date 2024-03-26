0a37bbaf974eed57afa37a2fbd8f4a9ff3e80bab
Nino Nikoles <nnikoles@gmail.com>
2024-03-26 17:43:04 +0100

    Create Logout.vue

---
+++ src/components/Logout.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="loader" class="">
+        <div class="content-wrap">
+            <i></i>
+            <span></span>
+        </div>
+    </div>
+</template>
+
+<script>
+import functions from './mixins/functions.vue';
+
+export default {
+    name: 'AppIndex',
+    mixins: [functions],
+    async mounted() {
+        this.logout_function();
+    }
+};
+</script>
