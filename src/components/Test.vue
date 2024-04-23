fee26b4b493378401224586df33304925a234f0e
Nino Nikoles <nnikoles@gmail.com>
2024-04-23 11:45:36 +0200

    Update Test.vue

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -4,6 +4,12 @@
             <p><button id="theme-switch" class="btn" @click="themeChange($event)">Change</button></p>
         </div>
 
+        <div class="col6 marg-right-col6">
+        
+
+            <!-- <button class="btn" @click="bearbeiteJSON">JSON Bearbeiten</button> -->
+        </div>
+
         <div class="col6">
             <h1>Lorem ipsum dolor sit amet.</h1>
             <h2>Lorem ipsum dolor sit amet.</h2>
@@ -86,11 +92,124 @@
 </template>
   
 <script>
+// import axios from 'axios';
+import 'cropperjs/dist/cropper.css';
 import functions from './mixins/functions.vue';
+import Cropper from 'cropperjs';
 // import ImageLoader from './includes/ImageLoader.vue';
 
 export default {
     name: 'AppTest',
-    mixins: [functions],
+    mixins: [functions, Cropper],
+data() {
+        return {
+            highlight: null,
+            availableGenre: null,
+            availableSlider: null,
+            availableSliderContent: null,
+            url: window.location.protocol + '//' + window.location.hostname,
+            allMedia: null,
+            userWatchList: null,
+            file: null,
+            selectedImg: null,
+            croppedImg: null,
+            cropperEl: null,
+            zoomVal: 0,
+            prevZoomVal: 0
+        };
+    },
+    methods: {
+        async onImgSelect(e) {
+            const file = e.target.files[0];
+            // Prüfe, ob ein Bild ausgewählt wurde
+            if (file && file.type.includes('image')) {
+                this.file = file;
+                // Erstelle eine temporäre URL für das Bild
+                this.selectedImg = URL.createObjectURL(file);
+
+                this.$nextTick(() => {
+                    this.cropper();
+                });
+            } else {
+                this.file = null;
+                this.imageUrl = null;
+                // Gib eine Fehlermeldung aus, falls kein Bild ausgewählt wurde
+                alert("Bitte wählen Sie ein Bild aus.");
+            }
+        },
+        cropper() {
+            const image = document.getElementById('image');
+
+            const cropper = new Cropper(image, {
+                viewMode: 0,
+                initialAspectRatio: 1/1,
+                aspectRatio: 1/1,
+                autoCropArea: 5,
+                zoomOnWheel: false,
+                crop() {
+                    // console.log(event.detail);
+                    this.croppedImg = cropper.getCroppedCanvas().toDataURL();
+                    // console.log(this.croppedImg);
+                },
+            });
+
+            this.cropperEl = cropper;
+        },
+        cropperScale(slider) {
+            const currentValue = parseInt(slider.target.value);
+            var zoomValue = currentValue/100+1;
+            this.cropperEl.scale(zoomValue);
+
+            this.prevZoomVal = currentValue;
+            this.zoomVal = currentValue;
+        },
+        async bearbeiteJSON() {
+            // JSON Datei URL
+            const jsonURL = '/media/test.json';
+            const response = await fetch(jsonURL);
+            console.log(response);
+            try {
+                // Daten laden
+                
+
+                if (response.ok) {
+                    // JSON Daten parsen
+                    const data = await response.json();
+                    console.log(data.uploads);
+
+                    // Neue Nummer generieren (die Länge des Arrays + 1)
+                    const neueNummer = data.uploads.length + 1;
+
+                    // Neuen Eintrag hinzufügen
+                    data.uploads.push({ "id": neueNummer, "image": 'Beispiel' });
+                    console.log(data);
+                    // Daten speichern
+                    await fetch(jsonURL, {
+                        method: 'POST',
+                        headers: {
+                        'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify(data)
+                    });
+                } else if (response.status === 404) {
+                // JSON Datei existiert nicht, also erstellen
+                const newData = {
+                    eintraege: [{ nummer: 1, string: 'Beispiel' }]
+                };
+
+                // Daten speichern
+                await fetch(jsonURL, {
+                    method: 'PUT',
+                    headers: {
+                    'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(newData)
+                });
+                }
+            } catch (error) {
+                console.error('Fehler beim Bearbeiten der JSON-Datei:', error);
+            }
+        },
+    },
 };
 </script>
