abd6a9a782881c3345dcd67a5676b9b5b9e31134
Nino Nikoles <nnikoles@gmail.com>
2024-04-23 11:40:33 +0200

    Update App.vue
    Header einbindung angepasst

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,27 @@
 <template>
     <div id="main">
-        <template v-if="$route.name !== 'Login' && $route.name !== 'Player' && $route.name !== 'PlayerMulti' && $route.name !== langSnippet('home') && $route.name !== langSnippet('movies') && $route.name !== langSnippet('shows') && $route.name !== langSnippet('my_list')">
-            <header-component></header-component>
-            <router-view></router-view>
-        </template>
-        <template v-else>
-            <header-component @mediaPopUp="updateVariable"></header-component>
-            <router-view @mediaPopUp="updateVariable"></router-view>
-        </template>
-        
+        <header-component></header-component>
+        <router-view></router-view>
     </div>
 
-    <template v-if="$route.name !== 'Player' && $route.name !== 'PlayerMulti'">
+    <template v-if="$route.path !== '/watch' && $route.name !== 'PlayerMulti'">
         <div id="callout" data-time="0" data-id="0">
             <p id="callout-content">test</p>
         </div>
-    </template>
 
-    <div class="modal" :id="`media-content`">
-        <div class="modal-overlay"></div>
-        
-        <div class="modal-wrap large">
-            <div class="modal-inner-wrap rounded">
-                <a href="#" class="modal-close" @click="closeMediaPopUp($event)"></a>
-                <media-content-popup :media="media" v-if="media"></media-content-popup>
+        <div class="modal" :id="`media-content`">
+            <div class="modal-overlay"></div>
+            
+            <div class="modal-wrap large">
+                <div class="modal-inner-wrap rounded">
+                    <a href="#" class="modal-close" @click="closeMediaPopUp($event)"></a>
+                    <!-- <media-content-popup :media="media" v-if="media"></media-content-popup> -->
+                </div>
             </div>
         </div>
-    </div>
+    </template>
+
+    <router-link style="position:fixed;z-index: 10000;" to="/watch?id=503314" title="Next">#Next</router-link>
 </template>
   
 <script>
@@ -35,15 +30,15 @@ import tmdbAPI from './components/mixins/tmdbAPI.vue';
 import functions from './components/mixins/functions.vue';
 import langSnippet from './components/mixins/language.vue';
 import { Fancybox } from "@fancyapps/ui";
-import MediaContentPopupComponent from './components/includes/MediaContentPopup.vue';
+// import MediaContentPopupComponent from './components/includes/MediaContentPopup.vue';
 
 export default {
     name: 'AppIndex',
     mixins: [functions, tmdbAPI, langSnippet],
-    emits: ['mediaPopUp'],
+    // emits: ['mediaPopUp'],
     components: {
         'header-component': Header,
-        'media-content-popup': MediaContentPopupComponent,
+        // 'media-content-popup': MediaContentPopupComponent,
     },
     data() {
         return {
