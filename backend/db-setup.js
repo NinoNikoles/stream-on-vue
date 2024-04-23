ac2f863629117426f39a94e90a2ba32f5e486147
Nino Nikoles <nnikoles@gmail.com>
2024-04-23 17:26:20 +0200

    Update db-setup.js
    Fix für eintrag überschreiben

--- backend/db-setup.js
+++ backend/db-setup.js
@@ -219,14 +219,15 @@ async function dbSetup() {
         return new Promise((resolve, reject) => {
             db.run(`CREATE TABLE IF NOT EXISTS media_watched (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-                user_id INTEGER UNIQUE,
-                media_id INTEGER UNIQUE,
-                show_id INTEGER NULL,
-                watched_seconds DECIMAL(10,6),
-                total_length DECIMAL(10,6),
-                watched INTEGER(1),
-                last_watched TIMESTAMP,
-                FOREIGN KEY (user_id) REFERENCES users(id)
+                user_id INTEGER,
+                media_id INTEGER,
+                show_id INTEGER,
+                watched_seconds DECIMAL(10, 6),
+                total_length DECIMAL(10, 6),
+                watched" INTEGER,
+                last_watched" TIMESTAMP,
+                FOREIGN KEY(user_id) REFERENCES users(id),
+                UNIQUE(user_id, media_id)
             )`, async function(err) {
                 if (err) {
                     reject(err);
