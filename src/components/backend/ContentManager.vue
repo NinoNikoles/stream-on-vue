7841a3bdf9148967fced99986f8ded82df59bba5
Nino Nikoles <nnikoles@gmail.com>
2024-06-10 17:13:03 +0200

    Create ContentManager.vue
    Created backend content manager page

---
+++ src/components/backend/ContentManager.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="col12 display-flex backend-wrap">
+
+        <backend-menu></backend-menu>
+
+        <div class="col12 backend-content pad-top-xl pad-bottom-l">
+            <div class="innerWrap">
+                <div class="col12">
+                    <h1>Content Manager</h1>
+                </div>
+
+                <div class="col12">
+                    
+                    <media-browser></media-browser>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import tmdbAPI from '../mixins/tmdbAPI.vue';
+import mainFunctions from '../mixins/functions.vue';
+import BackendMenu from '../includes/BackendMenu.vue';
+import ContentManager from './../MediaBrowser.vue';
+
+export default {
+    name: 'ContentManager',
+    mixins: [tmdbAPI, mainFunctions],
+    components: {
+        'backend-menu': BackendMenu,
+        'media-browser': ContentManager,
+    }
+};
+</script>
