0aec4ee7c4355de875372973e657459ae1ea33a2
Nino Nikoles <nnikoles@gmail.com>
2024-06-17 17:59:34 +0200

    Update MediaCard.vue
    Links angepasst

--- src/components/includes/MediaCard.vue
+++ src/components/includes/MediaCard.vue
@@ -22,7 +22,7 @@
             <div class="link-wrapper">
                 <progress v-if="getWatchedTime(media.watched_seconds, media.total_length)" max="100" :value="getWatchedTime(media.watched_seconds, media.total_length)"></progress>
                 <router-link v-if="media.file_path && media.media_type === 'movie'" :to="`/w?id=${media.tmdbID}`" :title="`${media.title}`" class="play-trigger"></router-link>
-                <router-link v-else-if="media.media_type === 'show' && media['episodes'][0].file_path" :to="`/watch?id=${media['episodes'][0].tmdbID}`" :title="`${media['episodes'][0].title}`" class="play-trigger"></router-link>
+                <router-link v-else-if="media.media_type === 'show' && media['episodes'][0].file_path" :to="`/w?id=${media['episodes'][0].tmdbID}`" :title="`${media['episodes'][0].title}`" class="play-trigger"></router-link>
                 
                 <button @click="openPopUp($event, id)" :title="langSnippet('more_informations')" class="info-trigger trigger-normal"></button>
                 <router-link v-if="$globalState.pageSettings[4].setting_option === 'true' && ($globalState.user.role ==='superadmin' || $globalState.user.role === 'admin')" :to="`/backend/${media.media_type}/${media.tmdbID}`" :title="langSnippet('edit')" class="edit-trigger"></router-link>
