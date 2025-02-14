f1ff10b7b56a687126b51455c8b3e838fcfb8a05
Nino Nikoles <nnikoles@gmail.com>
2025-02-14 11:32:42 +0100

    Update slider.js
    Better slider settings for viewports

--- src/functions/slider.js
+++ src/functions/slider.js
@@ -13,23 +13,30 @@ export function initSliders() {
         const swiper = new Swiper(slider, {
             loop: true,
             slidesPerView: 2,
+            slidesPerGroup: 2,
+            slidesPerGroupAuto: true,
             spaceBetween: 16,
             allowTouchMove: true,
             breakpoints: {
                 460: {
                     slidesPerView: 3,
+                    slidesPerGroup: 3,
                 },
                 720: {
                     slidesPerView: 4,
+                    slidesPerGroup: 4,
                 },
                 1080: {
                     slidesPerView: 6,
+                    slidesPerGroup: 6,
                 },
                 1280: {
                     slidesPerView: 8,
+                    slidesPerGroup: 8,
                 },
                 1400: {
                     slidesPerView: 10,
+                    slidesPerGroup: 10,
                 }
             },
             pagination: {
